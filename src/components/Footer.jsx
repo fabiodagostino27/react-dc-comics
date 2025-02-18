f9a868af10f3e988be5f5dd2b672b08b18d42ab4
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-18 18:12:55 +0100

    ADD: footer bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,9 @@
+import footFb from "../assets/img/footer-facebook.png";
+import footTw from "../assets/img/footer-twitter.png";
+import footYt from "../assets/img/footer-youtube.png";
+import footPin from "../assets/img/footer-pinterest.png";
+import footPer from "../assets/img/footer-periscope.png";
+
 const Footer = () => {
     return (
         <footer>
@@ -15,7 +21,7 @@ const Footer = () => {
                                 <li><a href="#">Videos</a></li>
                                 <li><a href="#">News</a></li>
                             </ul>
-                            
+
                             <h5>SHOP</h5>
                             <ul className="list-group">
                                 <li><a href="#">Shop DC</a></li>
@@ -50,6 +56,33 @@ const Footer = () => {
                         </div>
                     </div>
                     <figure className="w-50"></figure>
+                </div>
+
+            </div>
+            <div className="footer-bottom">
+                <div className="container py-4 d-flex justify-content-between">
+                    <a href="#" className="p-2">SIGN-UP NOW</a>
+                    <div>
+                        <div className="d-flex align-items-center gap-2">
+                            <span>FOLLOW US</span>
+                            <div className="d-flex gap-2">
+                                <a href="#">
+                                    <img src={footFb} alt="Facebook" />
+                                </a>
+                                <a href="#">
+                                    <img src={footTw} alt="Twitter" />
+                                </a><a href="#">
+                                    <img src={footYt} alt="YouTube" />
+                                </a>
+                                <a href="#">
+                                    <img src={footPin} alt="Pinterest" />
+                                </a>
+                                <a href="#">
+                                    <img src={footPer} alt="Periscope" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
