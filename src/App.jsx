a65e8c24c5c55781e4cb80fa4e6e5da8f20b112b
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-19 15:16:01 +0100

    ADD: array centralization

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
+    const navLinks = [
+        { id: 1, title: "CHARACTERS", url: "#", current: false },
+        { id: 2, title: "COMICS", url: "#", current: true },
+        { id: 3, title: "MOVIES", url: "#", current: false },
+        { id: 4, title: "TV", url: "#", current: false },
+        { id: 5, title: "GAMES", url: "#", current: false },
+        { id: 6, title: "COLLECTIBLES", url: "#", current: false },
+        { id: 7, title: "VIDEOS", url: "#", current: false },
+        { id: 8, title: "FANS", url: "#", current: false },
+        { id: 9, title: "NEWS", url: "#", current: false },
+        { id: 10, title: "SHOP", url: "#", current: false }
+    ]
+
     return (
-    <>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </>
-  )
+        <>
+            <Header navLinks={navLinks}/>
+            <Main />
+            <Footer />
+        </>
+    )
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,7 @@
 import logoImg from "../assets/img/dc-logo.png";
 
-const Header = () => {
-    const navLinks = [
-        { id: 1, title: "CHARACTERS", url: "#", current: false },
-        { id: 2, title: "COMICS", url: "#", current: true },
-        { id: 3, title: "MOVIES", url: "#", current: false },
-        { id: 4, title: "TV", url: "#", current: false },
-        { id: 5, title: "GAMES", url: "#", current: false },
-        { id: 6, title: "COLLECTIBLES", url: "#", current: false },
-        { id: 7, title: "VIDEOS", url: "#", current: false },
-        { id: 8, title: "FANS", url: "#", current: false },
-        { id: 9, title: "NEWS", url: "#", current: false },
-        { id: 10, title: "SHOP", url: "#", current: false }
-    ]
+const Header = (props) => {
+    const navLinks = props.navLinks;
 
     return (
         <header>
