f11ae14a85201d2a421ae40557aadd22399159b7
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-20 12:17:49 +0100

    FIX: comics' images urls

--- src/components/ComicsList.jsx
+++ src/components/ComicsList.jsx
@@ -1,7 +1,19 @@
 import comics from "../data/comics";
 
 const ComicsList = () => {
-    return ()
+    return (
+        comics.map(element => {
+            const {id, series, thumb} = element;
+            return (
+                <div key={id} className="col-2">
+                    <figure>
+                        <img className="w-100" src={thumb} alt={series} />
+                    </figure>
+                    <span>{series}</span>
+                </div>
+            )
+        })
+    )
 };
 
 export default ComicsList;
